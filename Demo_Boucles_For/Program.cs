3a43d7883c4d7248fcb2a17d14b86f185dd46e07
GlitchyMuon <glitchy.muon@proton.me>
2023-04-27 16:33:42 +0200

    Added bonus exercise text

--- Demo_Boucles_For/Program.cs
+++ Demo_Boucles_For/Program.cs
@@ -6,7 +6,7 @@
         {
             #region Demo for(..., ..., ...) simple boucle
 
-            const string PASSWORD = "Test1234=";
+            /*const string PASSWORD = "Test1234=";
             Console.WriteLine("Entrez votre mot de passe : ");
             string insert_password = Console.ReadLine();
             for (int i = 0; i < 3 && insert_password != PASSWORD; i++) 
@@ -20,10 +20,19 @@
             }
             else
             {
-                Console.WriteLine("Trop de tentatives ont échoués ...");
-            }
+                Console.WriteLine("Trop de tentatives ont échoués ...");*/
+        }
 
             #endregion
+
+            // Exo Bonus 
+
+            // A l'aide de deux boucles FOR, afficher les tables de multiplication de 1 à 9.
+            // Affichage :
+            // Table de 1 :
+            // 1 x 1 = 1 ; 1 x 2 = 2 ; 1 x 3 = 3 ; 1 x 4 = 4 ; 1 x 5 = 5 ; 1 x 6 = 6 ; 1 x 7 = 7 ; 1 x 8 = 8 ; 1 x 9 = 9 ; 1 x 10 = 10
+
+
         }
     }
 }
