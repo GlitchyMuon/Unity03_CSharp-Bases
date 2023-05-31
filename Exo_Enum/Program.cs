0e970ca60cd1c121fe791233ac913550a1e2fcbd
GlitchyMuon <glitchy.muon@proton.me>
2023-05-31 16:51:59 +0200

    modified Exo_Enum : correction

--- Exo_Enum/Program.cs
+++ Exo_Enum/Program.cs
@@ -10,10 +10,26 @@ namespace Exo_Enum
 
         public struct Carte
         {
-            public 
+            public ColorFamily couleurs;
+            public Values valeurs;
         }
         static void Main(string[] args)
         {
+            Carte[] deck = new Carte[52];
+            int i = 0;
+            foreach (string couleur in Enum.GetNames<ColorFamily>())
+            {
+                foreach (string valeur in Enum.GetNames<Values>()) 
+                {
+                    deck[i].valeurs = Enum.Parse<Values>(valeur);
+                    deck[i].couleurs = Enum.Parse<ColorFamily>(couleur);
+                    i++;
+                }
+            }
+            foreach (Carte carte in deck)
+            {
+                Console.WriteLine($"La carte du {carte.valeurs} de {carte.couleurs}");
+            }
             
         }
     }
